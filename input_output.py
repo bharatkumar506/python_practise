12f82f68330175601253af4ef4518dd95b2a8755
bharatkumar506 <bharatkumar506@gmail.com>
2021-12-15 16:42:21 +0530

    Initial Files

---
+++ input_output.py
@@ -0,0 +1,7 @@
+availableCash=int(input("Available cash:"));
+withdrawCash=int(input("Required to withdraw:"));
+remainingCash=availableCash-withdrawCash;
+print("Remaining Cash",remainingCash,sep=":")
+
+firstName, lastName=input("Enter Your First Name and last Name: ").split("@");
+print("My Name",firstName,lastName)
---
+++ print_options.py
@@ -0,0 +1,6 @@
+print('Hello','Bharat','Kumar',sep="-")
+print('Good Mornng',sep="-")
+
+
+print('Hello','Bharat','Kumar',sep="-",end="/")
+print('Good Mornng',sep="-")
